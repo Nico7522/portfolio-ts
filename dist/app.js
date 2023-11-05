15074a7e1a6f8f9dc0797ca31f9a2af2cf5d2330
Nicolas <nico.daddabbo7100@gmail.com>
2023-11-05 12:18:17 +0100

    fix import etc

--- dist/app.js
+++ dist/app.js
@@ -2,33 +2,28 @@
 // import * as AOS from 'aos';
 // import 'aos/dist/aos.css'; 
 // AOS.init();
-const menuHamburger = document.querySelector(".menu-h");
-const navLinks = document.querySelector(".nav-links");
-const SlidingNewsLetter = document.querySelector('.slide-in');
-const linkedinLogo = document.querySelector('.linkedin-logo');
-const githubLogo = document.querySelector('.github-logo');
-window.addEventListener('scroll', () => {
-    const { scrollTop, clientHeight } = document.documentElement;
-    // console.log(scrollTop, clientHeight);
-    console.log(scrollTop, SlidingNewsLetter === null || SlidingNewsLetter === void 0 ? void 0 : SlidingNewsLetter.getBoundingClientRect().top);
-    const fromTopElementTotopViewport = SlidingNewsLetter === null || SlidingNewsLetter === void 0 ? void 0 : SlidingNewsLetter.getBoundingClientRect().top;
+var menuHamburger = document.querySelector(".menu-h");
+var navLinks = document.querySelector(".nav-links");
+var SlidingNewsLetter = document.querySelector('.slide-in');
+var linkedinLogo = document.querySelector('.linkedin-logo');
+var githubLogo = document.querySelector('.github-logo');
+window.addEventListener("scroll", function () {
+    var _a = document.documentElement, scrollTop = _a.scrollTop, clientHeight = _a.clientHeight;
+    var fromTopElementTotopViewport = SlidingNewsLetter === null || SlidingNewsLetter === void 0 ? void 0 : SlidingNewsLetter.getBoundingClientRect().top;
     if (fromTopElementTotopViewport) {
-        const scrollTopAndFromTopElmentTotopViewport = Number((scrollTop + fromTopElementTotopViewport).toFixed());
-        if (scrollTop > scrollTopAndFromTopElmentTotopViewport - (clientHeight * 0.90)) {
-            SlidingNewsLetter === null || SlidingNewsLetter === void 0 ? void 0 : SlidingNewsLetter.classList.add('active');
+        var scrollTopAndFromTopElmentTotopViewport = Number((scrollTop + fromTopElementTotopViewport).toFixed());
+        if (scrollTop >
+            scrollTopAndFromTopElmentTotopViewport - clientHeight * 0.9) {
+            SlidingNewsLetter === null || SlidingNewsLetter === void 0 ? void 0 : SlidingNewsLetter.classList.add("active");
         }
     }
 });
-menuHamburger === null || menuHamburger === void 0 ? void 0 : menuHamburger.addEventListener('click', () => {
+menuHamburger === null || menuHamburger === void 0 ? void 0 : menuHamburger.addEventListener('click', function () {
     navLinks === null || navLinks === void 0 ? void 0 : navLinks.classList.toggle('mobile-menu');
 });
-linkedinLogo === null || linkedinLogo === void 0 ? void 0 : linkedinLogo.addEventListener('mouseover', () => {
-    if (githubLogo) {
-        githubLogo.style.width = '50px';
-    }
+linkedinLogo === null || linkedinLogo === void 0 ? void 0 : linkedinLogo.addEventListener("mouseover", function () {
+    githubLogo.style.width = "50px";
 });
-linkedinLogo === null || linkedinLogo === void 0 ? void 0 : linkedinLogo.addEventListener('mouseleave', () => {
-    if (githubLogo) {
-        githubLogo.style.width = '';
-    }
+linkedinLogo === null || linkedinLogo === void 0 ? void 0 : linkedinLogo.addEventListener("mouseleave", function () {
+    githubLogo.style.width = "";
 });
--- src/app.ts
+++ src/app.ts
@@ -8,35 +8,33 @@ const SlidingNewsLetter = document.querySelector('.slide-in');
 const linkedinLogo = document.querySelector('.linkedin-logo');
 const githubLogo = document.querySelector('.github-logo');
 
-window.addEventListener('scroll', () => {
-    const { scrollTop, clientHeight } = document.documentElement;
-    // console.log(scrollTop, clientHeight);
-console.log(scrollTop, SlidingNewsLetter?.getBoundingClientRect().top);
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
 
-    const fromTopElementTotopViewport = SlidingNewsLetter?.getBoundingClientRect().top;
-    if (fromTopElementTotopViewport) {
-        const scrollTopAndFromTopElmentTotopViewport: number = Number((scrollTop + fromTopElementTotopViewport).toFixed());
-        if (scrollTop > scrollTopAndFromTopElmentTotopViewport - (clientHeight* 0.90)) {
-            SlidingNewsLetter?.classList.add('active')
-        }
+
+  const fromTopElementTotopViewport =
+    SlidingNewsLetter?.getBoundingClientRect().top;
+  if (fromTopElementTotopViewport) {
+    const scrollTopAndFromTopElmentTotopViewport: number = Number(
+      (scrollTop + fromTopElementTotopViewport).toFixed()
+    );
+    if (
+      scrollTop >
+      scrollTopAndFromTopElmentTotopViewport - clientHeight * 0.9
+    ) {
+      SlidingNewsLetter?.classList.add("active");
     }
-    
-})
+  }
+});
 menuHamburger?.addEventListener('click', () => {
     navLinks?.classList.toggle('mobile-menu')
 });
 
 
-linkedinLogo?.addEventListener('mouseover', () => {
-    if (githubLogo) {
-        (githubLogo as HTMLElement).style.width = '50px'; 
-    }
-    
-})
+linkedinLogo?.addEventListener("mouseover", () => {
+  (githubLogo as HTMLElement).style.width = "50px";
+});
 
-linkedinLogo?.addEventListener('mouseleave', () => {
-    if (githubLogo) {
-        (githubLogo as HTMLElement).style.width = ''; 
-    }
-    
-})
+linkedinLogo?.addEventListener("mouseleave", () => {
+  (githubLogo as HTMLElement).style.width = "";
+});
